d4679a3894773cdfccf719cb32324d6620f15e48
PmaiWoW <PmaiWoW@Gmail.com>
2019-05-28 20:00:22 +0100

    Add skeleton of classes Character and Position

--- TrabalhoPratico2/Character.cs
+++ TrabalhoPratico2/Character.cs
@@ -4,7 +4,33 @@ using System.Text;
 
 namespace TrabalhoPratico2
 {
-    class Character
+    public abstract class Character
     {
+        // Variables and Properties
+        protected Position CharPosition;
+
+        // Método de Movimento (raio = 1) de corpo vazio, sendo que
+        // o zombie e humano herdam o método
+        public string Move()
+        {
+            // Verificar a vizinhança de Moore; se todas tiverem characters nas 
+            // mesmas, o movimento não é executado
+
+            // Caso possa executar o movimento, move-se
+
+            // Retorna string do Character movido (tipo de retorno sujeito a alteração)
+        }
+
+        // Método abstrato de Verificação da posição do humano/zombie mais próximo
+        public abstract Position VerifyOtherPosition(other) // other is placeholder
+        {
+            // Percorrer as 8 posições adjacentes (Moore de raio 1) ao character
+            // para verificar se alguma delas contém um character de tipo "contrário",
+            // aumentando o raio caso não encontre nenhum com o raio atual
+
+
+            // Caso encontre um character de tipo "contrário", retorna a posição do
+            // mesmo (? - tirar dúvida?)
+        }
     }
 }
---
+++ TrabalhoPratico2/Position.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrabalhoPratico2
+{
+    public class Position
+    {
+        // Posição (x, y)
+        protected int X { get; set; }
+        protected int Y { get; set; }
+
+        // Constructor
+        public Position(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
