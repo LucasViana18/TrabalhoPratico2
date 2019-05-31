76b467b7a9c2eff86a5dd8db7cc12203dc63f40a
PmaiWoW <PmaiWoW@Gmail.com>
2019-05-31 22:44:33 +0100

    Comment logic of TurnHuman() method Done with Lucas Viana

--- TrabalhoPratico2/Zombie.cs
+++ TrabalhoPratico2/Zombie.cs
@@ -4,7 +4,20 @@ using System.Text;
 
 namespace TrabalhoPratico2
 {
-    class Zombie : Character
+    public class Zombie : Character
     {
+        // Variables/Properties
+
+        public override Position VerifyOtherPosition(other)
+        {
+
+        }
+
+        public void TurnHuman()
+        {
+            // if the zombie detects a human in radius of 1, he attacks,
+            // turning him into a zombie (maybe delete said player and replace
+            // him with a zombie in the same position)
+        }
     }
 }
