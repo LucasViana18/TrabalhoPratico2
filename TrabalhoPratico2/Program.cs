1105ff572738d79964bc4233c6f66a1c9663f7ce
PmaiWoW <PmaiWoW@Gmail.com>
2019-06-01 13:43:45 +0100

    Add test code to create a new struct Params from the args (array of string) given from the command line

--- TrabalhoPratico2/Program.cs
+++ TrabalhoPratico2/Program.cs
@@ -7,6 +7,18 @@ namespace TrabalhoPratico2
         static void Main(string[] args)
         {
             Console.WriteLine("Zombie Game!");
+
+            Program Prog = new Program();
+            Params p = new Params();
+
+            p.ParseArgs(args);
+
+            Console.WriteLine($"X = {p.X}\nY = {p.Y}");
+
+            p.X += p.Y;
+            p.Y += p.X;
+
+            Console.WriteLine($"X = {p.X}\nY = {p.Y}");
         }
     }
 }
