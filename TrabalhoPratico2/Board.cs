49c16b1c3ffac9154bcde93208fd8c51d1b0b961
Lucas Viana <lucasvviana14@gmail.com>
2019-06-04 18:17:29 +0100

    Create and adjust Board class

--- TrabalhoPratico2/Board.cs
+++ TrabalhoPratico2/Board.cs
@@ -6,41 +6,88 @@ namespace TrabalhoPratico2
 {
     class Board
     {
-        // Variables
-        private Type[,] currentBoard;
-        private Type[,] background;
+        // Variables and Properties
+        private Params boardParams;
+        private GameElement[,] currentBoard;
+        private List<Zombie> zombies;
+        private List<Human> humans;
         private Random rnd;
 
-        // Constructor
-        public Board(int width, int height, int numOfHumans, int numOfZombies)
+        public int NumberColumns { get; private set; } = 0;
+        public int NumberRows { get; private set; } = 0;
+
+        public Board(Params p)
         {
             rnd = new Random();
-            background = new Type[width, height];
+            boardParams = p;
+            NumberColumns = boardParams.MaxX;
+            NumberRows = boardParams.MaxY;
+            currentBoard = new GameElement[NumberColumns, NumberRows];
+            zombies = new List<Zombie>();
+            humans = new List<Human>();
+        }
 
-            // Ciclos for para criar o tabuleiro vazio
-            for (int r = 0; r < height; r++)
+        public void StartBoard()
+        {
+            for (int r = 0; r < NumberRows; r++)
             {
-                for (int c = 0; c < width; c++)
+                for (int c = 0; c < NumberColumns; c++)
                 {
-                    // Fisher Yates Shuffle
-                    // rnd.Next();
-                    background[r, c] = Type.Empty;
-
+                    currentBoard[c, r] = new GameElement(c, r);
                 }
             }
 
-            currentBoard = new Type[width, height];
-
-            // Ciclos for para percorrer e adicionar por todo o tabuleiro os 
-            // humanos e zombies propostos
-
+            CreateZombies();
+            CreateHumans();
         }
 
-        /*
-        public Type VerifyNeighbors()
+        private Position FindFreeSpot()
         {
-            int radius = 1;
+            int localCol, localRow;
+            do
+            {
+                localCol = rnd.Next(0, NumberColumns - 1);
+                localRow = rnd.Next(0, NumberRows - 1);
+            } while (GetElementInPosition(localCol, localRow).ElementType != Type.Empty);
+
+            return new Position(localCol, localRow);
         }
-        */
+
+        private void CreateZombies()
+        {
+            Position localPosition;
+            Zombie localZombie;
+
+            for (int i = 0; i < boardParams.BotZ; i++)
+            {
+                localPosition = FindFreeSpot();
+
+                localZombie = new Zombie(localPosition.X, localPosition.Y, boardParams, i);
+                zombies.Add(localZombie);
+                currentBoard[localPosition.X, localPosition.Y] = localZombie;
+            }
+
+        }
+
+        private void CreateHumans()
+        {
+            Position localPosition;
+            Human localHuman;
+
+            for (int i = 0; i < boardParams.BotH; i++)
+            {
+                localPosition = FindFreeSpot();
+
+                localHuman = new Human(localPosition.X, localPosition.Y, boardParams, i);
+                humans.Add(localHuman);
+                currentBoard[localPosition.X, localPosition.Y] = localHuman;
+            }
+
+        }
+
+        public GameElement GetElementInPosition(int col, int row)
+        {
+            return currentBoard[col, row];
+        }
     }
 }
