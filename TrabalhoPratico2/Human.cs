6964d9551108988e5c508507c66f6c3305f35783
Guilherme Saturno <guilhermebsaturno@gmail.com>
2019-06-11 23:29:48 +0100

    Change AgentID to be created as hexadecimal

--- TrabalhoPratico2/Human.cs
+++ TrabalhoPratico2/Human.cs
@@ -11,41 +11,12 @@ namespace TrabalhoPratico2
 
         // Constructor
         public Human
-            (int startX, int startY, Params par, Board board, int lastAgentID):
-            base(startX, startY, par, board, lastAgentID)
+            (int startX, int startY, Params par, Board board, string agentID) :
+            base(startX, startY, par, board, agentID)
         {
             this.elementType = Type.Human;
             this.target = Type.Zombie;
         }
-        
-        /*
-        protected override void PathFinding(Board board)
-        {
-            base.PathFinding(board);
-
-            foreach (Position moorePos in possibleMoves)
-            {
-                bool validPos = true;
-                UpdateNeighR1(moorePos);
-
-                foreach (Position flowerPos in neighR1)
-                {
-                    foreach (Agent agent in board.agents)
-                    {
-                        if (flowerPos == agent.AgentPosition && agent.ElementType == Type.Zombie)
-                        {
-                            validPos = false;
-                        }
-                    }
-                }
-
-                if (validPos)
-                {
-                    agentMoves.Add(moorePos);
-                }
-            }
-        }
-        */
 
         public override string GetSymbol()
         {
